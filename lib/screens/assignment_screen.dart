5fa26b56a6cafcdf9bc93bf67312018bea8aa1b2
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2020-05-23 03:04:11 -0300

    Navegação no menu

--- lib/screens/assignment_screen.dart
+++ lib/screens/assignment_screen.dart
@@ -1,4 +1,5 @@
 import 'package:FiapEx/components/app_bar_fiap_ex.dart';
+import 'package:FiapEx/components/drawer_fiap_ex.dart';
 import 'package:flutter/material.dart';
 
 class AssignmentScreen extends StatelessWidget {
@@ -7,9 +8,19 @@ class AssignmentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarFiapEx(),
+      appBar: AppBarFiapEx(action: Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: InkWell(
+                 child: Image.asset('assets/images/pendenteicone.png',
+                          height: 26),
+                onTap: (){
+                  Navigator.of(context).pushReplacementNamed('/');
+                },
+              ),
+             )),
+      drawer: DrawerFiapEx(),
       body: Container(
-          color: Color(0xffff1819),
+          color: Theme.of(context).accentColor,
       ),
     );
   }
--- lib/screens/row_call_screen.dart
+++ lib/screens/row_call_screen.dart
@@ -8,7 +8,16 @@ class RowCallScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarFiapEx(),
+      appBar: AppBarFiapEx(action: Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: InkWell(
+                 child: Image.asset('assets/images/entregatrabalhos.png',
+                          height: 26),
+                onTap: (){
+                  Navigator.of(context).pushReplacementNamed('/assignment');
+                },
+              ),
+             )),
       drawer: DrawerFiapEx(),
       body: Container(
           color: Color(0xff151819),
