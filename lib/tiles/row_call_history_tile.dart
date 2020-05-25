2fe92f982456e3ac41b1560c7b6bcda2c231f98a
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2020-05-25 01:52:32 -0300

    historico de chamadas

---
+++ lib/tiles/row_call_history_tile.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class RowCallHistoryTile extends StatelessWidget {
+
+  bool done;
+
+  RowCallHistoryTile({Key key, this.done = false}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: (){
+        Navigator.of(context).pushReplacementNamed('/rowcall');
+      },
+      title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Desenvolvimento Cross Platform - 3SIT', style: TextStyle(fontSize:24)),
+                      Row(
+                        children: <Widget>[
+                          Text('Status: ', style: TextStyle(fontSize: 18)),
+                          Text('Pendente'),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text('Alunos: ', style: TextStyle(fontSize: 14)),
+                          Text('40', style: TextStyle(fontSize: 16) ),
+                          SizedBox(width: 10,),
+                          Text(
+                            'Ausentes: ',
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          Text('12', style: TextStyle(fontSize: 16)),
+                          SizedBox(width: 10,),
+                          Text('Presentes: ', style: TextStyle(fontSize: 14)),
+                          Text('28 ', style: TextStyle(fontSize: 16)),
+                        ],
+                      ),
+                      Text('24/05/2020', style: TextStyle(fontSize: 15))
+                    ],
+                  ),
+                  leading: done? Image.asset(
+                    'assets/images/checkmark.png',
+                    width: 40,
+                  ) :
+                  Image.asset(
+                    'assets/images/pending.png',
+                    width: 40,
+                  ),
+                  
+              );
+  }
+}
